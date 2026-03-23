e3650af95dcaa2e179f1fb9f028f40a4874f2cdc
abdulhadishajeeb <abdulhadishajeeb@gmail.com>
2026-03-23 18:28:06 +0530

    supabase edit

--- proxify-full/proxify-supabase/src/supabase.js
+++ proxify-full/proxify-supabase/src/supabase.js
@@ -4,7 +4,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://placeholder.supabase.co'
-const SUPABASE_ANON_KEY = 'placeholder'
+const SUPABASE_URL = 'https://sgpgkkgepkecnjqefzaz.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNncGdra2dlcGtlY25qcWVmemF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNDgwNDEsImV4cCI6MjA4OTgyNDA0MX0.3W33F5n3KfujV_7pTbBBWP8nFXREtYXv2SkxC-zN-Jk'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
